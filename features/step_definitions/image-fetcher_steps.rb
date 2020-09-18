86f46bfb2a950ab01021a74702ba90875ac2f410
Johannes Engl <hello@johannesengl.com>
2020-09-18 09:02:13 +0200

    Remove cucumber and test rake task

--- features/step_definitions/image-fetcher_steps.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-When(/^I get help for "([^"]*)"$/) do |app_name|
-  @app_name = app_name
-  step %(I run `#{app_name} help`)
-end
-
-# Add more step definitions here
